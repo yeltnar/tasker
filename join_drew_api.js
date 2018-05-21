042989f9a8760343453c7663572f8d17629fd0c4
yeltnar <andcbrant@gmail.com>
2018-05-21 16:06:40 -0500

    Update join_drew_api.js

--- join_drew_api.js
+++ join_drew_api.js
@@ -1,3 +1,10 @@
 //flashLong("You have got the file");
 
 flashLong(data);
+
+var action = data.action;
+
+if( action === "performTask" ){
+  var taskName = data.taskName;
+  performTask(taskName,5);
+}

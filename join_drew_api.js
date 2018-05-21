2d5268f81e31817f9042cd50173295a5d9f7970e
yeltnar <andcbrant@gmail.com>
2018-05-21 16:28:18 -0500

    added updateScript

--- join_drew_api.js
+++ join_drew_api.js
@@ -4,12 +4,16 @@ try{
   data = JSON.parse(data);
   var action = data.action;
   
-  if( action === "performTask" ){
+  if(action === "performTask"){
     var taskName = data.taskName;
 
     flashLong("taskName "+taskName+" performTask "+performTask);
     performTask(taskName,5);
 
+  }
+  else if(action === "updateScript"){
+    deleteFile( "Tasker/join_drew_api.js", 0, false );
+    performTask("Join Command Here",5);
   }else{
     flash("didn't match: action "+action+" data "+JSON.stringify(data));
 }

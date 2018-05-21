9e3691014251633688ab7e5520dcd893d0a13bf3
yeltnar <andcbrant@gmail.com>
2018-05-21 16:18:34 -0500

    Update join_drew_api.js

--- join_drew_api.js
+++ join_drew_api.js
@@ -1,12 +1,20 @@
 //flashLong("You have got the file");
 
-var action = data.action;
-
-if( action === "performTask" ){
-  var taskName = data.taskName;
+try{
+  data = JSON.parse(data);
+  var action = data.action;
   
-  flashLong("taskName "+taskName+" performTask "+performTask);
-  performTask(taskName,5);
-}else{
-  flash("didn't match: action "+action+" data "+JSON.stringify(data));
+  if( action === "performTask" ){
+    var taskName = data.taskName;
+
+    flashLong("taskName "+taskName+" performTask "+performTask);
+    performTask(taskName,5);
+
+  }else{
+    flash("didn't match: action "+action+" data "+JSON.stringify(data));
 }
+}catch(e){
+  
+}
+  
+

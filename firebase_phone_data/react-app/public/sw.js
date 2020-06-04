eea58775e9354b39e3f097e9f66739aef13ed977
Drew Brantley <brantley@us.ibm.com>
2020-06-04 15:02:45 -0500

    add caching with service worker

--- firebase_phone_data/react-app/public/sw.js
+++ firebase_phone_data/react-app/public/sw.js
@@ -1,15 +1,76 @@
-/* This work is licensed under the W3C Software and Document License
- * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
- */
+const version = "0.2";
+const cacheName = `notifications-${version}`;
+
+self.addEventListener('install', event => {
+
+  var cachesToKeep = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cachesToKeep.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/notifications_manager/',
+      ])
+      .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
 self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    // Get content from the network.
-    try {
-      return await fetch(event.request);
-    } catch (e) {
-      // Failure. Just return a 200 page, to satisfy Lighthouse.
-      return new Response('You are offline :(', {status: 200});
-    }
-  })());
+
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true, ignoreVary: true}))
+      .then(response => {
+        console.log({response_defined:response!==undefined})
+
+        if( isDataRequest(event.request.url) ){
+          return fetch(event.request);
+        }
+
+        if(response!==undefined){
+          return response
+        }else{
+          addCache(event.request.url);
+          return fetch(event.request);
+        }
+      })
+  );
 });
+
+function addCache(url_to_add){
+
+  console.log(`have not saved ${url_to_add}`)
+
+  if( !/^https?:\/\//.test(url_to_add) ){
+    return // do not try to cache addons
+  }
+  
+  caches.open(cacheName).then(cache => {
+    return cache.addAll([
+      url_to_add
+    ])
+    // .then(() => self.skipWaiting());
+  })
+}
+
+function isDataRequest( url ){
+  return /^https:\/\/node.andbrant.com/.test(url);
+}
+
+function removeCache(url_to_remove){
+  throw new Error("removeCache not ready");
+}
--- firebase_phone_data/react-app/src/serviceWorker.js
+++ firebase_phone_data/react-app/src/serviceWorker.js
@@ -20,7 +20,18 @@ const isLocalhost = Boolean(
     )
 );
 
-export function register(config) {
+export function register() {
+
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register(`${process.env.PUBLIC_URL}/sw.js`)
+            //  .register('/sw.js')
+             .then(function() { console.log("Service Worker Registered"); });
+  }  
+
+}
+
+function old_register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
